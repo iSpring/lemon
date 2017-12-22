cd81147de44b1b160065d144f568637049ed717c
Sun Qun <1039219852@qq.com>
2017-12-22 22:38:05 +0800

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(logger('dev', {
   }
 }));
 app.use(bodyParser.json({limit: '1mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compression());
 var RedisStore = require('connect-redis')(session);
